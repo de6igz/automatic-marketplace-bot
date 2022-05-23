04daa305a404f7000980c786b1a5ffc6bfebc9c4
de6igz <Solopov2003@gmail.com>
2022-05-23 18:47:43 +0300

    работает просмотр анкет, добавление товаров, но только для верхней одежды

--- bot.py
+++ bot.py
@@ -10,10 +10,12 @@ bot = telebot.TeleBot(gitignore.tokens.token)
 sqlite_connection = sqlite3.connect('database.db', check_same_thread=False)
 cursor = sqlite_connection.cursor()
 sqlite_connection.commit()
+temp_user_form_description = {}
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    temp_user_form_description[message.from_user.id] = 'temp'
     buyer_or_seller_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     buyer_or_seller_markup.add('Я покупатель', 'Я продавец')
     bot.send_message(message.chat.id,
@@ -33,6 +35,20 @@ def main(message):
         show_buyer_categories(message)
     if message.text == 'Назад в главное меню':
         bot.send_message(message.chat.id, 'Главное меню', reply_markup=buyer_or_seller_markup)
+    if message.text == 'Мои анкеты':
+        show_my_products(message)
+
+
+def show_my_products(message):
+    user_id = str(message.from_user.id)
+    cursor.execute(f"select description from products where product_ID like '%{user_id}%'")
+    my_products = cursor.fetchall()
+    counter = 1
+    for row in my_products:
+        img = open(f'products/image_{user_id}_{counter}.jpg', 'rb')
+        bot.send_message(message.chat.id, f'{row[0]}')
+        bot.send_photo(message.chat.id, img)
+        counter = counter + 1
 
 
 def show_buyer_categories(message):
@@ -86,7 +102,7 @@ def check_form(message):
             telegram_tag = re.search(r'@\w+', form).group(0)
             if re.search(r'\d+ рублей', form):
                 msg = bot.send_message(message.chat.id, 'Теперь отправьте фото товара')
-                cursor.execute(f"insert into products(description) values ('{form}')")
+                temp_user_form_description[message.from_user.id] = form
                 bot.register_next_step_handler(msg, download_picture)
             else:
                 bot.send_message(message.chat.id, 'Не корректный ввод стоимости, попробуйте еще раз создать заявку')
@@ -106,27 +122,34 @@ def check_form(message):
 
 @bot.message_handler(content_types=['photo'])
 def download_picture(message):
-    nomer = 0
-    cursor.execute(f'select number_of_products from sellers where telegram_tag like "{message.from_user.id}"')
-    temp = cursor.fetchall()
-    for row in temp:
-        nomer = row[0]
-    nomer = int(nomer)
-    print(nomer, type(nomer))
-    print('message.photo =', message.photo)
-    fileID = message.photo[-1].file_id
-    print('fileID =', fileID)
-    file_info = bot.get_file(fileID)
-    print('file.file_path =', file_info.file_path)
-    downloaded_file = bot.download_file(file_info.file_path)
-    nomer = nomer + 1
-    cursor.execute(f'update sellers set number_of_products = {nomer} where telegram_tag like "{message.from_user.id}"')
-    sqlite_connection.commit()
-    with open(f"products/image_{message.from_user.id}_{nomer}.jpg", 'wb') as new_file:
-        new_file.write(downloaded_file)
-        new_file.close()
-    cursor.execute(f"insert into products(photo_link,product_ID) values ('products/image_{message.from_user.id}_{nomer}.jpg','{message.from_user.id}_{nomer}')")
-    sqlite_connection.commit()
+    try:
+        nomer = 0
+        cursor.execute(f'select number_of_products from sellers where telegram_tag like "{message.from_user.id}"')
+        temp = cursor.fetchall()
+        for row in temp:
+            nomer = row[0]
+        nomer = int(nomer)
+        print(nomer, type(nomer))
+        print('message.photo =', message.photo)
+        fileID = message.photo[-1].file_id
+        print('fileID =', fileID)
+        file_info = bot.get_file(fileID)
+        print('file.file_path =', file_info.file_path)
+        downloaded_file = bot.download_file(file_info.file_path)
+        nomer = nomer + 1
+        cursor.execute(
+            f'update sellers set number_of_products = {nomer} where telegram_tag like "{message.from_user.id}"')
+        sqlite_connection.commit()
+        with open(f"products/image_{message.from_user.id}_{nomer}.jpg", 'wb') as new_file:
+            new_file.write(downloaded_file)
+            new_file.close()
+        cursor.execute(
+            f"insert into products(description,photo_link,product_ID) values ('{temp_user_form_description[message.from_user.id]}','products/image_{message.from_user.id}_{nomer}.jpg','{message.from_user.id}_{nomer}')")
+        sqlite_connection.commit()
+        bot.send_message(message.chat.id, '<i>Товар успешно добавлен✅</i>', parse_mode='HTML')
+    except Exception as e:
+        bot.send_message(message.chat.id, 'Не корректный формат изображения')
+        creating_form(message)
 
 
 def creating_underwear_form(message):
